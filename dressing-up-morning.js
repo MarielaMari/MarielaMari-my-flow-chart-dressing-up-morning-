6e57f483a8277cddddd685857854213d47e3fa48
Mariela <mvmihilova@gmail.com>
2022-10-08 23:55:33 -0600

    did split the code on smaller pieces

--- dressing-up-morning.js
+++ dressing-up-morning.js
@@ -1,24 +1,41 @@
-const dressingUpInMorining = (amIGoingOut, isItRaining, isItSnowing, isItWarm) => {
+const goingOut = (amIGoingOut) => {
+    if (amIGoingOut) {
+        console.log('Am i going out?', amIGoingOut);
+        console.log('Check the weather.');
+    }
+}
 
-if (amIGoingOut) {
-    console.log('Am i going out?', amIGoingOut);
-    console.log('Check the weather.');
-
+const raining = (isItRaining) => {
     if (isItRaining) {
         console.log('Is it raining?', isItRaining);
         console.log('Put raining set of clothes on and get out.');
     }
-        if (isItSnowing) {
-            console.log('Is it snowing?', isItSnowing);
-            console.log('Put snowing set of clothes on and get out.')
-        }
-            if (isItWarm) {
-                console.log('Is it warm?', isItWarm);
-                console.log('Put clothes for warm weather on and get out.');
-            }
-                
 }
+
+const snowing = (isItSnowing) => {
+    if (isItSnowing) {
+        console.log('Is it snowing?', isItSnowing);
+        console.log('Put snowing set of clothes on and get out.')
+    }
+}
+
+const warm = (isItWarm) => {
+    if (isItWarm) {
+        console.log('Is it warm?', isItWarm);
+        console.log('Put clothes for warm weather on and get out.');
+    }
+}
+
+
+
+const dressingUpInMorining = (amIGoingOut, isItRaining, isItSnowing, isItWarm) => {
+goingOut(amIGoingOut);
+raining(isItRaining);
+snowing(isItSnowing);
+warm(isItWarm);               
 };
+
+
 
 console.log('\n');
 dressingUpInMorining(true, true, false, false);
